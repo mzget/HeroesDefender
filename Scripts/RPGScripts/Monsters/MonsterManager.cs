7005c5d8400dc61a35fec3a79ee4555a7ae44b33
ohmdob <ohm10513@hotmail.com>
2013-03-03 01:35:35 +0700

    update monster manager

--- Scripts/RPGScripts/Monsters/MonsterManager.cs
+++ Scripts/RPGScripts/Monsters/MonsterManager.cs
@@ -3,21 +3,39 @@ using System.Collections;
 
 public class MonsterManager : Base_CharacterBeh {
 
+	public bool _isAlive = true;
 	private bool _isStarting = false;
-
-//    private AIMonster aiMonster;
-//	private IDMonster idMonster;
+	private Vector2 hpBarSize;
+	private float monsterSpeed = 0.4f;
+	private bool tempFlip;
+	private bool enableAttack = true;
+	public float MonsterSpeed
+	{
+		get { return monsterSpeed; }
+        set { monsterSpeed = value; }
+	}
+	private bool flip = false;
+	public bool Flip
+	{
+		get { return flip; }
+        set { flip = value; }
+	}
 	
+	void Awake(){
+		if(flip){
+			this.animatedSprite.scale = new Vector3(-1f,1f,1f);
+		}
+		tempFlip = Flip;
+	}
+	
+//  private AIMonster aiMonster;
+//	private IDMonster idMonster;
 
 	// Use this for initialization
 	protected override void Start ()
 	{
 		base.Start ();
-
-		maxHP = 300;
-		hp = maxHP;
-		base.InitailizeHUDData ();
-
+		this._isAlive = true;
 		this.animState = AnimationState.idle;
 		this.animationManager.PlayAnimationByName (CharacterAnimationManager.NameAnimationsList.Idle);
 
@@ -43,9 +61,28 @@ public class MonsterManager : Base_CharacterBeh {
 		base.Update ();
 		
 		if(_isStarting && this.animState == AnimationState.walk) {
-			this.transform.Translate(Vector3.left * WaveManager.EnemySpeed, Space.World);
+			
+			if(flip!=tempFlip){
+				this.animatedSprite.scale = new Vector3(-1f,1f,1f);
+			}else{
+				this.animatedSprite.scale = new Vector3(1f,1f,1f);
+			}
+			if(this.tag!="Monster"){
+				this.transform.Translate(Vector3.right * monsterSpeed, Space.World);
+			}else{
+				this.transform.Translate(Vector3.left * monsterSpeed, Space.World);
+			}
+			
+		}else if(this.animState == AnimationState.attack){
+			if(targetEnemy!=null){
+				if(targetEnemy.transform.position.x > this.gameObject.transform.position.x){
+					this.animatedSprite.scale = new Vector3(-1f,1f,1f);
+				}else{
+					this.animatedSprite.scale = new Vector3(1f,1f,1f);
+				}
+			}
 		}
-		
+
 		/// Check Dying Event.
 //		if (idMonster.hp <= 0 && _isAlive)
 //		{
@@ -80,55 +117,122 @@ public class MonsterManager : Base_CharacterBeh {
     #endregion
 	
 	#region <@-- Handle Collision Event.
+
+
 	
+	bool isRange(GameObject targetEnemy)
+	{
+		float mePos = this.transform.position.x;
+		float targetPos = targetEnemy.transform.position.x;
+		float range =  Mathf.Abs(mePos-targetPos);	
+		Debug.Log(range);
+		if(range<80f){
+			return true;
+		}
+		return false;
+	}
+	IEnumerator atk(){
+		yield return new WaitForSeconds(0.5f);
+		this.animState = AnimationState.attack;
+		if(enableAttack && targetEnemy){
+			enableAttack = false;
+			this.animationManager.PlayAnimationByName (CharacterAnimationManager.NameAnimationsList.Attack);	
+			this.animatedSprite.animationCompleteDelegate = (sprite, clipId) => {
+				targetEnemy.SendMessage("ReceiveDamage", 10f, SendMessageOptions.DontRequireReceiver);
+				this.animationManager.PlayAnimationByName (CharacterAnimationManager.NameAnimationsList.Attack);
+				enableAttack = true;	
+				};
+		}
+	}
+	IEnumerator exit(){
+		enableAttack = false;
+		yield return new WaitForSeconds(0.5f);
+		enableAttack = true;
+		this.animState = AnimationState.walk;
+		this.animationManager.PlayAnimationByName (CharacterAnimationManager.NameAnimationsList.Walk);
+		targetEnemy = null;
+	}
 	void OnTriggerEnter (Collider coll)
-	{
-//		if (coll.tag == "Hero" || coll.tag == "Unit") {
-//			if(this.gameObject.tag == "Unit") {
-//				// Ignore.
-//			}
-//			else {
-//				if (targetEnemy == null) {
-//					targetEnemy = coll.gameObject;
-//					if (this.animState != AnimationState.dead) {
-//						this.animState = AnimationState.attack;
-//						this.animationManager.PlayAnimationByName (CharacterAnimationManager.NameAnimationsList.Attack);
-//						this.animatedSprite.animationCompleteDelegate = (sprite, clipId) => {
-//							if(targetEnemy) {
-//								targetEnemy.SendMessage("ReceiveDamage", 10f, SendMessageOptions.DontRequireReceiver);
-//								this.animationManager.PlayAnimationByName (CharacterAnimationManager.NameAnimationsList.Attack);
-//							}
-//						}; 
-//					}   
-//				}
-//			}
-//			
-//			Debug.Log (coll.tag);
-//		}
+	{	
+		if (collider.tag == "Hero" || collider.tag == "Unit") {
+			Debug.Log(coll.gameObject.tag);
+			if(coll.gameObject.tag=="Monster"){
+				targetEnemy = coll.gameObject;
+				StartCoroutine(this.atk());
+			}
+		}else if (collider.tag == "Monster"){
+			if(coll.gameObject.tag!="Monster"){
+				targetEnemy = coll.gameObject;
+				StartCoroutine(this.atk());
+			}
+		}
 	}
 	
-	void OnTriggerStay(Collider coll)
+	void OnTriggerExit (Collider coll)
 	{
-	}
-	
-	void OnTriggerExit (Collider collider)
-	{
-//		if (collider.tag == "Hero" || collider.tag == "Unit") {
-//			targetEnemy = null;
-//			if(animState != AnimationState.dead || animState != AnimationState.attack) {
-//				this.animState = AnimationState.walk;
-//				this.animationManager.PlayAnimationByName(CharacterAnimationManager.NameAnimationsList.Walk);
-//			}
-//		}
+		StartCoroutine(this.exit());
+		//
+		//Debug.Log(this.gameObject.name+" exit");
+		//targetEnemy = coll.gameObject;
+		//StartCoroutine(this.CheckTarget(this.gameObject.name,coll.gameObject.name));
+
+		/*
+		if (coll.tag == "Hero" || coll.tag == "Unit") {
+			if(this.gameObject.tag == "Unit") {
+				Debug.Log(this.gameObject.tag);
+				// Ignore.
+			}
+			else  {
+				if (targetEnemy == null) {
+					targetEnemy = coll.gameObject;
+					StartCoroutine(this.CheckTarget(this.gameObject.name,coll.gameObject.name));
+				}
+			}
+		}*/
+		/*
+		this.animState = AnimationState.walk;
+		if (collider.tag == "Hero") {
+			targetEnemy = null;
+			//if(animState != AnimationState.walk) {
+				this.animState = AnimationState.walk;
+				this.animationManager.PlayAnimationByName(CharacterAnimationManager.NameAnimationsList.Walk);
+			//}
+		}
+		else if (collider.tag == "Monster")
+		{
+		}
+		*/
 	}
 	
 	#endregion
 
-	protected override void Handle_deadAnimationComplete (tk2dAnimatedSprite sprite, int clipId)
-	{
-		base.Handle_deadAnimationComplete (sprite, clipId);
+    /// <summary>
+    /// HookUp By Simple Event.
+    /// </summary>
+    public void ShowMonsterName()
+    {
+//        textMeshName.text = idMonster.fixName;
+//        textMeshName.transform.position = this.gameObject.transform.position + Vector3.up * ((this.animationSprite.size.y/2) + 40);
 
-		WaveManager.Arr_monsterManager.Remove (this);
-		Destroy (this.gameObject);
-	}
+//        /// Set PowerBar Position.
+//        hpBar_Ins.active = true;
+//        powerBar_Ins.active = true;
+//        powerBarSprite.transform.position = this.transform.position + new Vector3(0, (this.animationSprite.size.y/2) + 20, 0);
+//        hpBarSprite.transform.position = this.transform.position + new Vector3(-50, (this.animationSprite.size.y/2) + 20, 0);
+    }
+	
+    public void ReceiveDamage(int r_Damage)
+    {
+//    	idMonster.hp -= r_Damage;
+        SetHealth();
+    }
+
+    private void SetHealth()
+    {
+//        if (hpBarSprite)
+//        {
+//            float hpBarScale = idMonster.hp * 100f / idMonster.maxHP;
+//            hpBarSprite.size = new Vector2(hpBarScale, hpBarSprite.size.y);
+//        }
+    }
 }
--- Scripts/RPGScripts/Monsters/WaveManager.cs
+++ Scripts/RPGScripts/Monsters/WaveManager.cs
@@ -5,11 +5,12 @@ using System.Collections.Generic;
 public class WaveManager : MonoBehaviour {
 
 	public static List<MonsterManager> Arr_monsterManager = new List<MonsterManager>();
+	public static Hashtable GC_Monster = new Hashtable();
+	
 	GameObject EnemyGroup;
 	public float startTime;
 	public int arrWidth;
 	public int arrHeight;
-	public static float EnemySpeed = 0.3f;
 	public const string PATH_OF_ENEMY_01 = "Prototypes/Monsters/Monster";
 	public bool EnableUpdate = true;
 	
@@ -17,18 +18,18 @@ public class WaveManager : MonoBehaviour {
 	private float [] zAxis = new float[] { -3f, -4f, -5f, -6f, -7f};
 	private int [,] wavePattern = new int[,] 
 	{
-		{0 ,1 ,0 ,0 ,0 ,1 ,0 ,1 },
-		{0 ,1 ,0 ,0 ,1 ,0 ,1 ,0 },
-		{1 ,0 ,0 ,0 ,1 ,1 ,1 ,1	},	
-		{0 ,0 ,1 ,1 ,0 ,1 ,0 ,0 },
-		{0 ,0 ,0 ,1 ,1 ,0 ,1 ,1	}
+		{1 ,0 ,0 ,0 ,0 ,0 ,0 ,0 },
+		{0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 },
+		{0 ,0 ,0 ,0 ,0 ,0 ,0 ,0	},	
+		{0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 },
+		{0 ,0 ,0 ,0 ,0 ,0 ,0 ,0	}
 	};			
 	public int [,] WavePattern
    	{
         get { return wavePattern; }
         set { wavePattern = value; }
     }
-	private float initDelay = 2.0f;
+	private float initDelay = 1.0f;
 	public float InitDelay
 	{
 		get { return initDelay; }
@@ -47,22 +48,50 @@ public class WaveManager : MonoBehaviour {
 		//zeroInit(wavePattern);
 		startTime = Time.time;
 		EnemyGroup = new GameObject("EnemyGroup");
+		
+		string monsterName = "Unit1";
+		GameObject clone = Instantiate(Resources.Load(PATH_OF_ENEMY_01, typeof(GameObject))) as GameObject;
+		clone.transform.position = new Vector3(-120f, yAxis[0], zAxis[0]);
+		clone.name = monsterName;
+		clone.tag = "Hero";
+		clone.GetComponent<MonsterManager>().Flip = true;
 	}
 	
 	// Use this for initialization
 	void Start () {	
 		WaveInit(wavePattern,EnemyGroup);
+		
 	}
 
 	void Update () {
 		WaveUpdate(wavePattern);
 	}
 	
+	void WaveInit(int [,] pattern, GameObject objGroup) {
+		for(int i =0; i < arrWidth ; i++) {
+			for(int j=0; j < arrHeight; j++) {
+				if(pattern[i,j]!=0) {
+					int waveIndex = ((arrWidth*j)+i);
+					string monsterName = "Enemy"+waveIndex.ToString();
+					GC_Monster.Add(monsterName, waveIndex);
+					GameObject clone = Instantiate(Resources.Load(PATH_OF_ENEMY_01, typeof(GameObject))) as GameObject;
+					clone.name = monsterName;
+					clone.transform.parent = objGroup.transform;
+					clone.transform.position = new Vector3(130f, yAxis[i], zAxis[i]);
+					
+					clone.tag = "Monster";
+					MonsterManager cloneBeh = clone.GetComponent<MonsterManager>();
+					WaveManager.Arr_monsterManager.Add(cloneBeh);
+				}
+			}
+		}
+	}
+	
 	void WaveUpdate(int [,] pattern) {
 		if(EnableUpdate)
 		{
-			float elapsed = Time.time - startTime;
 			int index = 0;
+			float elapsed = Time.time - startTime;
 			//int hours 	= (int)(elapsed / 3600);
 			//int minutes = (int)(elapsed / 60);
 	 		//int seconds = (int)(elapsed % 60);
@@ -72,35 +101,36 @@ public class WaveManager : MonoBehaviour {
 					if(elapsed>(j*delayPerWave+initDelay)) {
 						if(pattern[i,j]!=0){
 							GameObject updateEnemy = GameObject.Find("Enemy"+((arrWidth*j)+i));
-//							updateEnemy.transform.Translate(EnemySpeed,0f,0f);
-							if(updateEnemy)
-								updateEnemy.GetComponent<MonsterManager>().StartWalking();
+							updateEnemy.GetComponent<MonsterManager>().StartWalking();
 						}
 					}
 					index++;
-					// if update all unit
+						//if update all unit
 					if(index==(arrWidth*arrHeight)-1){
+						WaveDestroy(pattern);
 						//EnableUpdate = false;
+						GameObject updateUnit = GameObject.Find("Unit1");
+						updateUnit.GetComponent<MonsterManager>().StartWalking();
 					}
 				}
 			}
 		}
 	}
 	
-	void WaveInit(int [,] pattern, GameObject objGroup) {
-		for(int i =0; i < arrWidth ; i++) {
-			for(int j=0; j < arrHeight; j++) {
-				if(pattern[i,j]!=0) {
-					GameObject clone = Instantiate(Resources.Load(PATH_OF_ENEMY_01, typeof(GameObject))) as GameObject;
-					clone.name = "Enemy"+((arrWidth*j)+i);
-					clone.transform.parent = objGroup.transform;
-					clone.transform.position = new Vector3(330f, yAxis[i], zAxis[i]);
-					
-					clone.tag = "Monster";
-					MonsterManager cloneBeh = clone.GetComponent<MonsterManager>();
-					WaveManager.Arr_monsterManager.Add(cloneBeh);
-				}
+	void WaveDestroy(int [,] pattern){
+		List<string> removeHash = new List<string>(); 
+		foreach (DictionaryEntry Item in GC_Monster)
+		{	
+	       	if(GameObject.Find(Item.Key.ToString()).transform.position.x <= -80f){
+				int wi = ((int)Item.Value / arrWidth);
+				int wj = ((int)Item.Value % arrWidth);
+				wavePattern[wj,wi] = 0;
+				Destroy(GameObject.Find(Item.Key.ToString()));
+				removeHash.Add(Item.Key.ToString());
 			}
+		}
+		foreach(string key in removeHash){
+			GC_Monster.Remove(key);
 		}
 	}
 	
